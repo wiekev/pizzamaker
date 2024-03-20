cd4d41a430a462ac5c7b72342daa13e5e05cc0aa
wiekev <152178391+wiekev@users.noreply.github.com>
2024-03-20 13:12:43 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -4,65 +4,74 @@ let mozza = document.querySelector("#mozzarella")
 let olij = document.querySelector("#olijf")
 let sala = document.querySelector("#salami")
 
+let aansaus = false
+let aantomaat = false
+let aanmozzarella = false
+let aanolijf = false
+let aansalami = false
+
 function saus(){
-    if(aan === true){
-        aan = false
+    if(aansaus === true){
+        aansaus = false
         document.querySelector("#sauslos").style.display = "none";
     } else {
-        aan = true
+        aansaus = true
         document.querySelector("#sauslos").style.display = "block";
     }
 }
 
+tomSaus.addEventListener("click", saus)
+
 function tom(){
-    if(aan === true){
-        aan = false
+    if(aantomaat === true){
+        aantomaat = false
         document.querySelector("#tomaatlos").style.display = "none";
     } else {
-        aan = true
+        aantomaat = true
         document.querySelector("#tomaatlos").style.display = "block";
     }
 }
 
+tomaat2.addEventListener("click", tom)
+
 function mozza2(){
-    if(aan === true){
-        aan = false
+    if(aanmozzarella === true){
+        aanmozzarella = false
         document.querySelector("#mozzarellalos").style.display = "none";
     } else {
-        aan = true
+        aanmozzarella = true
         document.querySelector("#mozzarellalos").style.display = "block";
     }
 }
 
+mozza.addEventListener("click", mozza2)
+
 function olijf2(){
-    if(aan === true){
-        aan = false
+    if(aanolijf === true){
+        aanolijf = false
         document.querySelector("#olijflos").style.display = "none";
     } else {
-        aan = true
+        aanolijf = true
         document.querySelector("#olijflos").style.display = "block";
     }
 }
 
+olij.addEventListener("click", olijf2)
+
 function salami2(){
-    if(aan === true){
-        aan = false
+    if(aansalami === true){
+        aansalami = false
         document.querySelector("#salamilos").style.display = "none";
     } else {
-        aan = true
+        aansalami = true
         document.querySelector("#salamilos").style.display = "block";
     }
 }
 
-
-tomSaus.addEventListener("click", saus)
-tomaat2.addEventListener("click", tom)
-mozza.addEventListener("click", mozza2)
-olij.addEventListener("click", olijf2)
 sala.addEventListener("click", salami2)
 
-let aan = false
-    
+
+
 
 let getal = document.querySelector("p")
 let klaar = document.querySelector(".klaar")
@@ -83,3 +92,16 @@ function nogEenKeer(){
 }
 
 opnieuw.addEventListener("click", nogEenKeer)
+
+let jee = document.querySelector(".yay")
+let boo = document.querySelector(".boe")
+
+function geluid(){
+    if(cijfer > 5){
+jee.play()
+    } else{
+boo.play()
+    }
+}
+
+klaar.addEventListener("click", geluid)
